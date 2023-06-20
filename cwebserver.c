5900b0c3b3b901ff68e118cc613aa10214b4d5a9
khoahuynhdev <khoa0319@gmail.com>
2023-06-20 22:22:02 +0700

    feat: add buffer for copying from kernel

--- cwebserver.c
+++ cwebserver.c
@@ -12,6 +12,16 @@
 int main () {
   // define the server and client file descriptors
   int server_fd, client_fd;
+  // the client_fd is only get created when a connection is established to that socket
+  // a client_fd == a connection
+  // server_fd 1-m client_fd
 
+  // define the socket address
+  struct sockaddr_in address;
+  int address_len = sizeof(address);
+
+  // define the application buffer where we receives the requests
+  // data will be moved from received buffer to here
+  char buffer[APP_MAX_BUFFER] = {0}; // the entire buffer will be init to zero
   return 0;
 }
