95b7d7e5dcc48b7da77b9e4a2bae9ee4f6ec9ae7
khoahuynhdev <khoa0319@gmail.com>
2023-06-20 22:09:28 +0700

    initial commit

---
+++ cwebserver.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+//maximum application buffer
+#define APP_MAX_BUFFER 1024
+#define PORT 8080
+
+int main () {
+  // define the server and client file descriptors
+  int server_fd, client_fd;
+
+  return 0;
+}
